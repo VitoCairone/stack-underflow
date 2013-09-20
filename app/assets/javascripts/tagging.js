5810690e3ad67fe7da4db1717be625d0a193fc64
Vito Cairone <vitocairone@gmail.com>
2013-09-20 16:05:59 -0700

    Finished bug which prevented tagging.js from running properly

--- app/assets/javascripts/tagging.js
+++ app/assets/javascripts/tagging.js
@@ -1,13 +1,16 @@
-//This causes no tag to be selected by default in the tag lists
-$(".question-tag-select").prop("selectedIndex", -1);
+$(document).ready(function () {
+  //This causes no tag to be selected by default in the tag lists
+  $(".question-tag-select").prop("selectedIndex", -1);
 
-//This unhides subsequent tag selectors when a tag is selected.
-//TODO: Allow 'closing' of tag selectors
-//TODO: Don't display options in later tag selectors
-//      which were already chosen
-$(".question-tag-select").on("change", function() {
-	var my_number = this.id.slice(-1);
-	next_number = parseInt(my_number) + 1;
-	$next_el = $("#question-tag-ids-" + next_number);
-	$next_el.removeClass("hidden");
+  //This unhides subsequent tag selectors when a tag is selected.
+  //TODO: Allow 'closing' of tag selectors
+  //TODO: Don't display options in later tag selectors
+  //      which were already chosen
+  $(".question-tag-select").on("change", function() {
+  	var my_number = this.id.slice(-1);
+  	next_number = parseInt(my_number) + 1;
+  	$next_el = $("#question-tag-ids-" + next_number);
+    console.log($next_el);
+  	$next_el.removeClass("hidden");
+  });
 });
